27f5fdbb3759a05a92a5e681beabf56aecf25ae3
Juri <jur13_5@web.de>
2020-12-18 00:38:56 +0100

    MainMenu und Settings (Grafik, Sound usw.) Script erstellt

--- Assets/Scripts/BuildHeap.cs
+++ Assets/Scripts/BuildHeap.cs
@@ -9,80 +9,52 @@ public class BuildHeap : MonoBehaviour
 {
     [SerializeField]
     public Transform[] target = new Transform[15];
-    
-  //  public Vector3[] targetPos = new Vector3[15];
     [SerializeField]
     public Transform[] curr = new Transform[15];
-   
-    
     public Transform collTarget;
-   
-    public bool done = false;
-   
-    
-    public Button yourButton;
 
-    public int k = 0;
+    public int g = 0;
+    public float speed = 12.0f;
 
-   
 
-    public float speed = 2.0f;
-
-    void Awake()
+    void Start()
     {
-        
+
+        StartCoroutine(MoveItems());       //Starts routine to move objects
+
     }
 
-        void Start()
-        {
-            Button btn = yourButton.GetComponent<Button>();
-            btn.onClick.AddListener(TaskOnClick);
-            StartCoroutine(MoveToColl());               //Starts routine to move objects
-         }
 
 
 
-
-        void Update()
-        {
-            if (done)
-            {
-                StartCoroutine(MoveToTarget());        //Not good, read comments at line 72/73
-                
-            }
-
-         }
-
-
-    IEnumerator MoveToColl()            //TEST > Move to collision cube 
+    void Update()
     {
-        foreach(Transform cp in curr) { 
+
+
+    }
+
+
+    IEnumerator MoveItems()                 //Move sequence
+    {
+
+        foreach (Transform cp in curr)                                                                                  //From Startpoint to CollisionTrigger
+        {
             while (cp.position != collTarget.position)
             {
-                cp.position = Vector3.MoveTowards(cp.position, collTarget.position, speed * Time.deltaTime);
+                cp.position = Vector3.MoveTowards(cp.position, collTarget.position, (speed + 5) * Time.deltaTime);
                 yield return null;
-              
             }
-            
+
+            //------------------------------------------------------------------------------------------------------------//
+
+            while (curr[g].position != target[g].position)                                                               //From CollisionTrigger to TargetPoints
+            {
+                curr[g].position = Vector3.MoveTowards(curr[g].position, target[g].position, speed * Time.deltaTime);
+                yield return null;
+            }
+            g++;
+            yield return new WaitForSeconds(.5f);
         }
-        done = true;
     }
-
-
-    IEnumerator MoveToTarget()  //TEST > Move from collision cube to targetpoints
-                                //need to find a way to loop over both arrays via foreach, like in MoveToColl so one element at a time moves.
-    {
-       for(int i = 0; i<target.Length; i++) {
-               curr[i].position = Vector3.MoveTowards(curr[i].position, target[i].position, speed * Time.deltaTime);
-                 yield return null;
-        }
-
-}
-
-
-    void TaskOnClick()  //For test purposes not needed
-        {
-        
-        }
 
 }
