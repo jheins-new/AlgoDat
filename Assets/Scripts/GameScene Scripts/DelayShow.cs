285f54601bf82bc53f34a21beb4f822841d8a5f0
Nick <nick.goehlert@web.de>
2021-01-05 18:37:08 +0100

    aktueller stand

---
+++ Assets/Scripts/GameScene Scripts/DelayShow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Delay for some time to show the game object.
+/// </summary>
+public class DelayShow : MonoBehaviour
+{
+    public float showTime;
+    bool done = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!done)
+        {
+            if (showTime <= Time.time)
+            {
+               // this.GetComponentInChildren<TextMesh>().GetComponent<Renderer>().enabled = false;
+                Renderer[] renderers = this.GetComponentsInChildren<Renderer>();
+                 foreach (var item in renderers)
+                 {
+                     item.enabled = true;
+                 }
+                //TextMesh[] textMeshes = this.GetComponentsInChildren<TextMesh>();
+                // foreach (var item in textMeshes)
+                // {
+                //     item.GetComponent<Renderer>().enabled = true;
+                // }
+
+                // {
+                //     Transform valueNode = this.transform.Find("Value");
+                //     valueNode.GetComponent<Renderer>().enabled = true;
+                //     TextMesh textMesh = valueNode.GetComponentInChildren<TextMesh>();
+                //     textMesh.GetComponent<Renderer>().enabled = true;
+                // }
+                // {
+                //     Transform indexNode = this.transform.Find("Index");
+                //     indexNode.GetComponent<Renderer>().enabled = true;
+                //     TextMesh textMesh = indexNode.GetComponentInChildren<TextMesh>();
+                //     textMesh.GetComponent<Renderer>().enabled = true;
+                // }
+
+                done = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameScene Scripts/btnDefaultCase.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btnDefaultCase : MonoBehaviour
+{
+
+  public static int[] itemCost = new int[15];
+  public static Transform[] itemHolder = new Transform[15];
+  [SerializeField]
+  public Transform[] setItemHolder = new Transform[15];
+
+
+  void Awake()
+  {
+
+    for (int i = 0; i < setItemHolder.Length; i++)
+    {
+      itemHolder[i] = setItemHolder[i];
+    }
+
+  }
+  public void btnDefaultCase_Click()
+  {
+    //Test data
+    itemCost[0] = 32;
+    itemCost[1] = 53;
+    itemCost[2] = 3;
+    itemCost[3] = 65;
+    itemCost[4] = 99;
+    itemCost[5] = 42;
+    itemCost[6] = 17;
+    itemCost[7] = 333;
+    itemCost[8] = 133;
+    itemCost[9] = 33;
+    itemCost[10] = 733;
+    itemCost[11] = 633;
+    itemCost[12] = 833;
+    itemCost[13] = 443;
+    itemCost[14] = 213;
+
+  }
+}
